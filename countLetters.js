dbec34b0f88f018dc90f5d079f4d9b907a224418
mahadabdi9 <mahadabdi9@gmail.com>
2023-02-08 19:51:50 -0500

    function that counts the letters in an array

---
+++ countLetters.js
@@ -0,0 +1,20 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (str) {
+  const result = {};
+  const newString = str.split(" ").join("");
+  for (const letter of newString) {
+    if (result[letter]) {
+      result[letter] += 1;
+    } else {
+      result[letter] = 1;
+    }
+  }
+  return result;
+};
