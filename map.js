1844858ab17d3aeb42e226f55855848ff645d3b0
mahadabdi9 <mahadabdi9@gmail.com>
2023-02-09 22:03:28 -0500

    function that maps through array with a callback function

---
+++ map.js
@@ -0,0 +1,32 @@
+function eqArrays(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅  Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, (word) => word[1]);
+console.log(results1);
+
+assertArraysEqual(eqArrays(results1, ["r", "o", "o", "a", "o"]), true); //PASS
