f4716f56e34ab45869b8c6d30b2fe654fc1f58a3
mahadabdi9 <mahadabdi9@gmail.com>
2023-02-13 14:54:44 -0500

    uploading lotide files

---
+++ findKeyByValue.js
@@ -0,0 +1,11 @@
+const findKeyByValue = function (obj, value) {
+  const keys = Object.keys(obj)
+  for (const key of keys) {
+    if (obj[key] === value) {
+      return key;
+    }
+  }
+}
+
+let result = findKeyByValue({a:1, b:2, c:3, d:4}, 3);
+console.log(result);
